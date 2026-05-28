f22b648c31e664a808fd0ef67346ccf9f6c3e54f
LucianaMenotti <lucianamenottiipf@gmail.com>
2026-05-28 12:04:32 -0300

    Se termino la actividad1

---
+++ actividad1.js
@@ -0,0 +1,9 @@
+const nombre = "Menotti Luciana";
+let edad = 19;
+const carrera =
+  "Tecnicatura Superior de Desarrollo de Software Multiplataforma";
+let estado = true;
+
+console.log(
+  `Hola, me llamo ${nombre}, tengo ${edad} años. Actualmente estoy cursando la carrera ${carrera} y mi estado es ${estado}.`,
+);
